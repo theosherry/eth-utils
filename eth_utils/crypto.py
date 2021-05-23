d5b8f18d3902c5ef345734df0eb145900735787e
theosherry <sherr.theo@gmail.com>
2021-05-23 04:31:29 -0400

    remove eth-hash dependency

--- eth_utils/crypto.py
+++ eth_utils/crypto.py
@@ -1,6 +1,5 @@
 from typing import Union
 
-from eth_hash.auto import keccak as keccak_256
 
 from .conversions import to_bytes
 
@@ -8,4 +7,4 @@ from .conversions import to_bytes
 def keccak(
     primitive: Union[bytes, int, bool] = None, hexstr: str = None, text: str = None
 ) -> bytes:
-    return keccak_256(to_bytes(primitive, hexstr, text))
+    raise NotImplementedError('Removed to avoid circular reference. Use eth-hash directly'.)
